c5a780696a2b2532f3911ab7583bedcc12c4919e
Moab07Alves <moab.alves@dcx.ufpb.br>
2024-10-31 09:28:29 -0300

    FINALIZADO SEÇÃO 2 DO CURSO DE PHP

---
+++ 2_CONCEITOS_BASICOS/4_COMENTARIOS/index.php
@@ -0,0 +1,14 @@
+<?php
+
+// Este é um comentário
+echo "teste comentario"; //Isso aqui imprimi algo na tela
+
+# Outro tipo de comentário
+
+/* 
+    Comentário 
+    Multi
+    Linha
+*/
+
+?>
---
+++ 2_CONCEITOS_BASICOS/5_EXERCICIO_2/index.php
@@ -0,0 +1,20 @@
+<?php
+
+/*  
+    Meu nome é Moab Alves de Souza, tenho 20 anos e estou fazendo o curso de PHP
+    Sou Estudante da Univesidade Federal da Paraíba e faço o curso de Sistemas da informação em Rio Tinto
+    Pretendo ser um bom programador e utilizar as ferramentas do PHP para criar os meus sistemas
+*/
+
+$nome = "Moab Alves de Souza"; // Esta variável contém o nome da pessoa
+$idade = 20; // Essa variável contém a idade da pessoa
+$universidade = "Universidade Federal da Paraíba"; // Essa variável contém a universidade da pessoa
+$curso = "Sistemas da Informação"; // Essa variável contém o curso da pessoa
+$cidade = "Rio Tinto"; // Essa variável contém a cidade onde fica a universidade da qual a pessoa faz o curso
+$profissao = "Programador"; // Essa variável contém a profissão da pessoa
+
+echo "Meu nome é $nome, tenho $idade anos e estou fazendo o curso de PHP <br>";
+echo "Sou Estudante da $universidade e faço o curso de $curso em $cidade <br>";
+echo "Pretendo ser um bom $profissao e utilizar as ferramentas do PHP para criar os meus sistemas <br>";
+
+?>
